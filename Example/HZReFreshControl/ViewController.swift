fe7fa783eda741c7ed63d3f3a19fffda91399fd2
zyfMac <zyfmac@zyfMacdeMac-mini.local>
2022-11-08 17:05:22 +0800

    使用说明

--- Example/HZReFreshControl/ViewController.swift
+++ Example/HZReFreshControl/ViewController.swift
@@ -12,6 +12,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+     //   let tableView = UITableView.init(frame: .zero, style: .plain)
+//        tableView.headerRefreshControl?.headerFreshControlEvent = {
+//
+//
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak self] in
+//                self?.tableView.reloadData()
+//                self?.tableView.headerRefreshControl?.headerEndRefresh()
+//            }
+//        }
+//
+//        tableView.bottomRefreshControl?.bottomRefreshEvent = {
+//           // sleep(2)
+//
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak self] in
+//                self?.tableView.reloadData()
+//                self?.tableView.bottomRefreshControl?.bottomEndRefresh()
+//            }
+//        }
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
